4598a6b13bdc8b5fde38a8ab8d8fd3c4d7bfce97
Camila Pinzon <camilapinz18@gmail.com>
2023-07-09 18:40:46 -0500

    return new reservtions obj

--- app/modules/reservations/providers/reservation.py
+++ app/modules/reservations/providers/reservation.py
@@ -10,20 +10,43 @@ from app.core.db.default_data.master_data.roles.roles import DefaultRoles
 
 class Reservation():
     def get_reservations(current_user, db_session):
-        reservations=None
+        reservations = None
 
         if current_user['role'] == DefaultRoles.client.code:
             reservations = db_session.query(ReservationModel).filter(
-                ReservationModel.client_id==current_user['id']).all()
-            
+                ReservationModel.client_id == current_user['id']).all()
+
         if current_user['role'] == DefaultRoles.admin.code:
             reservations = db_session.query(ReservationModel).all()
 
-        return reservations
-    
+        reservation_to_return = []
+
+        for r in reservations:
+            print("R", r.id)
+
+            obj = {
+                'id': r.id,
+                'url': '',
+                'title': str(r.start_hour) + ' ' + str(r.end_hour),
+                'time': {
+                    'start': str(r.date) + ' '+str(r.start_hour),
+                    'end': str(r.date) + ' ' + str(r.end_hour),
+                },
+                'tags': r.reservation.name,
+                'location': r.room.name,
+                'client':r.client.id
+
+            }
+
+            reservation_to_return.append(obj)
+
+        print("reservation_to_return", reservation_to_return)
+
+        return reservation_to_return
 
     def get_reservation_by_id(id, db_session):
-        reservation = db_session.query(ReservationModel).filter(ReservationModel.id == id).first()
+        reservation = db_session.query(ReservationModel).filter(
+            ReservationModel.id == id).first()
 
         if not reservation:
             raise HTTPException(
@@ -32,53 +55,51 @@ class Reservation():
             )
 
         return reservation
-    
-    def create_reservation(reservation, db_session):
-        equipments=reservation.equipments
-        
-        
-        reservation = ReservationModel(**reservation.dict(exclude_unset=True, exclude={"equipments","room"}))
-        #reservation.equipments=equipments
 
-        total_equipments=[]
+    def create_reservation(reservation, db_session):
+        equipments = reservation.equipments
+
+        reservation = ReservationModel(
+            **reservation.dict(exclude_unset=True, exclude={"equipments", "room"}))
+        # reservation.equipments=equipments
+
+        total_equipments = []
 
         for id in equipments:
-            print("id",id)
+            print("id", id)
 
             equipment_objects = db_session.query(EquipmentModel).filter(
-                EquipmentModel.id==id).first()
-            
+                EquipmentModel.id == id).first()
+
             if not equipment_objects:
                 raise HTTPException(
                     status_code=404,
                     detail='No se ha encontrado un equipo con el id proporcionado'
                 )
-            
+
             total_equipments.append(equipment_objects)
-        
-        print("equipment_objects",equipment_objects)
-        reservation.equipments=total_equipments
+
+        print("equipment_objects", equipment_objects)
+        reservation.equipments = total_equipments
 
         db_session.add(reservation)
         db_session.commit()
-        #db_session.refresh(reservation)
-        
+        # db_session.refresh(reservation)
+
         # equipments_to_save=[]
-        
+
         # for data in equipments:
         #     reservation_equipments=ReservationEquipmentsModel(
         #         equipment_id = data,
         #         reservation_id = reservation.id
         #     )
-            
+
         #     equipments_to_save.append(reservation_equipments)
-            
+
         # db_session.add_all(equipments_to_save)
         # db_session.commit()
-        
+
         return {"msg": f"Se han creado la reservación exitosamente"}
-
-
 
     def delete_room_by_id(id, db_session):
         room = db_session.query(RoomModel).filter(RoomModel.id == id).first()
@@ -94,7 +115,7 @@ class Reservation():
             )
 
     def update_room(id, room_update, db_session):
-        
+
         room = db_session.query(RoomModel).filter(RoomModel.id == id).first()
 
         if not room:
@@ -106,7 +127,7 @@ class Reservation():
         room.name = room_update.name
         room.status = room_update.status
         room.category_name = room_update.category_name
-        
+
         db_session.add(room)
         db_session.commit()
 
