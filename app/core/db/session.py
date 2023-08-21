f2361f9fcb0d27a7553823c91c6a6390b88559b0
Camila Pinzón <83082880+Camilapinz18@users.noreply.github.com>
2023-08-20 20:17:27 -0500

    Update session.py

--- app/core/db/session.py
+++ app/core/db/session.py
@@ -9,7 +9,8 @@ from app.core.data import Data
 engine = create_engine(
     #f'postgresql://Camilapinz18:gR6oQeOBftJ9@ep-muddy-snowflake-466588.us-east-2.aws.neon.tech/neondb',
     #f'postgresql://sabseg_admin:sabseg@localhost:5432/sabseg_db',
-    'postgresql://fl0user:yL4Oq5CeEPDW@ep-cool-glade-16399750.us-east-2.aws.neon.tech:5432/sabseg?sslmode=require')
+    f'postgresql://Camilapinz18@ep-white-leaf-37502486.us-east-2.aws.neon.tech/neondb')
+    #'postgresql://fl0user:yL4Oq5CeEPDW@ep-cool-glade-16399750.us-east-2.aws.neon.tech:5432/sabseg?sslmode=require')
     #f'postgresql://Camilapinz18:fa1W7HZykIQT@ep-white-leaf-37502486.us-east-2.aws.neon.tech/neondb')
 
 
