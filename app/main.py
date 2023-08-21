7f96d9646b4ecdd69b30ec1411675615857ed970
Camila Pinzón <83082880+Camilapinz18@users.noreply.github.com>
2023-08-20 20:38:11 -0500

    upgrade

--- app/main.py
+++ app/main.py
@@ -5,17 +5,45 @@ import sys
 
 from app.core.db.default_data.import_bulk_default_data import import_bulk_default_data
 
+# lib
+import shutil
+import time
+import os
+
+
+# sqlalchemy
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+# alembic
+from alembic.migration import MigrationContext
+from alembic.config import Config
+from alembic import command
+
+# Set up the Alembic configuration object
+alembic_cfg = Config(file_="alembic.ini")
+alembic_cfg.set_main_option('script_location', 'app/alembic')
+alembic_cfg.set_main_option('url', f'postgresql://Camilapinz18:swh6GdoLqz8W@ep-white-leaf-37502486.us-east-2.aws.neon.tech/neondb')
+
+
+
 app = FastAPI(
     title='SABSEG-FastAPI',
     openapi_url=f"{'/api/v1'}/openapi.json"
 )
 
+def generate_upgrade_head():
+    print("upgrading")
+    command.upgrade(alembic_cfg, revision="head")
+    
 
 @app.on_event("startup")
 async def startup_event():
 
-    print(sys.path)
+    #print(sys.path)
+    generate_upgrade_head()
     #import_bulk_default_data()
+    print("started")
 
 app.add_middleware(
     CORSMiddleware,
